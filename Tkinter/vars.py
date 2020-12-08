a66a0bdd3bf405548ac05b1ec128e3cbc5e9752d
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-12-08 15:31:16 +0530

    Remove PyQt5 and Tkinter folders

--- Tkinter/vars.py
+++
@@ -1,8 +0,0 @@
-#-- code: utf-8 -- 
-company_name = "Maharashtra Decorators"
-sqllite_db_file = 'db/sqllite.db'
-delete_icon = "images/delete.png"
-update_icon = "images/update.png"
-
-bill_seperator = "--------------------------------------------\
---------------------------------------------------------------------------------------------------"
